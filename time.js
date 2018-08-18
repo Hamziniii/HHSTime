52df7cbb8e1e1a8ea09afbd36118a58d07645d5d
Hamziniii <hamzagaziuddin04@gmail.com>
2018-08-17 20:43:51 -0500

    Update time.js

--- time.js
+++ time.js
@@ -1,6 +1,6 @@
-(function(){
-a = 'PLTW, AP Physics 1, Algebra 2 Honors, French 1, Lunch, Health, English 1 Honors, AP Human Geography' // PUT CLASSES HERE SEPERATED BY SPACE. IF YOUR CLASS HAS A SPACE REPLACE IT WITH AN UNDERSCORE
-b = 'W1317, W1016, W2009, C1020, W1203, W1307, W1200, W1208' // PUT YOUR ROOM NUMBERS HERE AND SEPERATE THEM BY A SPACE
+javascript(function(){
+a = '' // PUT CLASSES HERE SEPERATED BY SPACE. IF YOUR CLASS HAS A SPACE REPLACE IT WITH AN UNDERSCORE
+b = '' // PUT YOUR ROOM NUMBERS HERE AND SEPERATE THEM BY A SPACE
 let classes = a.split(", ")
 let rooms = b.split(", ")
 // let classes = ['PLTW', 'AP Physics 1', 'Algebra 2 Honors', 'French 1', 'Lunch', 'Health', 'English 1 Honors', 'AP Human Geography']
