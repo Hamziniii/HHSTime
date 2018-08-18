19bd2a659d9be62abd9db3fcd4ebdf180effa13d
Hamziniii <hamzagaziuddin04@gmail.com>
2018-08-17 20:02:23 -0500

    even more friendlier to programming noobs

--- time.js
+++ time.js
@@ -1,8 +1,8 @@
 (function(){
-a = '' // PUT CLASSES HERE SEPERATED BY SPACE. IF YOUR CLASS HAS A SPACE REPLACE IT WITH AN UNDERSCORE
-b = '' // PUT YOUR ROOM NUMBERS HERE AND SEPERATE THEM BY A SPACE
-let classes = a.split(" ").map(e => e.split('_').join(' '))
-let rooms = b.split(" ")
+a = '' // PUT YOUR CLASSES HERE SEPERATED BY COMMA AND SPACE
+b = '' // PUT YOUR ROOM NUMBERS HERE SEPERATED BY COMMA AND SPACE
+let classes = a.split(", ")
+let rooms = b.split(", ")
 // let classes = ['PLTW', 'AP Physics 1', 'Algebra 2 Honors', 'French 1', 'Lunch', 'Health', 'English 1 Honors', 'AP Human Geography']
 // let rooms = ['W1317', 'W1016', 'W2009', 'C1020', 'W1203', 'W1307', 'W1200', 'W1208']
 let num = 0
