dc7e0d4b369b24eaca1d1a3a0ff1884b9bf7d925
Hamziniii <hamzagaziuddin04@gmail.com>
2018-09-11 09:41:25 -0500

    Update bk.js

--- bk.js
+++ bk.js
@@ -1,1 +1,1 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.rawgit.com/Hamziniii/HHSTime/master/time.js';})();
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.rawgit.com/Hamziniii/HHSTime/master/timenc.js';})();
